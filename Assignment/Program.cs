37f89e9e90cc6220b2a3637b18f2419ea8d391c4
hoang <hgle249305@gmail.com>
2026-01-27 16:22:03 +0700

    Add goodbye message

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,10 +4,30 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Nhan phim bat ky de hien thong bao...");
+        Console.WriteLine("Nhan phim bat ky de bat dau...");
         Console.ReadKey();
+        Console.WriteLine();
 
-        Console.WriteLine("xin chào");
+        Console.WriteLine("Nhan 1 de hien 'xin chào'");
+        Console.WriteLine("Nhan 2 de hien 'tạm biệt'");
+        Console.Write("Lua chon cua ban: ");
+
+        char choice = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (choice == '1')
+        {
+            Console.WriteLine("xin chào");
+        }
+        else if (choice == '2')
+        {
+            Console.WriteLine("tạm biệt");
+        }
+        else
+        {
+            Console.WriteLine("Lua chon khong hop le");
+        }
+
         Console.ReadKey();
     }
 }

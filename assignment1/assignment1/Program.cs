653310c5fef4b21bd159e69039414d2407a8834f
hungntm <hungminhml26@gmail.com>
2026-01-29 08:44:48 +0700

    update xinchao

--- assignment1/assignment1/Program.cs
+++ assignment1/assignment1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+Console.WriteLine("===== MENU =====");
+Console.WriteLine("1. Xin chào");
+Console.Write("select option: ");
+
+string? choice = Console.ReadLine();
+
+if (choice == "1")
+{
+    Console.WriteLine("xin chào");
+}
+
+Console.ReadKey();
+
